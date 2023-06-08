b7894c20de886c25eaf539aaad54a2a7c512e223
Alheser <135245468+Alheser@users.noreply.github.com>
2023-06-08 17:14:44 +0300

    Update Ping-pong.py

--- Ping-pong.py
+++ Ping-pong.py
@@ -30,12 +30,43 @@ window.fill((255, 255, 255))
 clock = time.Clock()
 game = True
 finish = False
+ball = GameSprite('tenis_ball.png', 250, 250, 5, (50,50))
+Player1 = Player('racket.png', 5, 150, 5, (50,150))
+Player2 = Player('racket.png', 550, 150, 5, (50,150))
+
+speed_x = 7
+speed_y = 7
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE', True, (180, 0, 0))
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.fill((255, 255, 255))
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        Player1.reset()
+        Player1.update_l()
+        Player2.reset()
+        Player2.update_r()
+        ball.reset()
+    if ball.rect.y > 450 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(Player1, ball) or sprite.collide_rect(Player2, ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200,200))
+    if ball.rect.x > 550:
+        finish = True
+        window.blit(lose2, (200,200))
+
     display.update()
     clock.tick(60) 
-
+    
 
 
